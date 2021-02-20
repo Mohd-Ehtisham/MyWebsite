78579af16039742f8571029b8df8120958b782a0
Mohd-Ehtisham <mohammedehtisham108@gmail.com>
2021-02-20 22:48:33 +0530

    second commit

--- src/About.jsx
+++ src/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <Common
                 name="Welcome Come To About Page"
                 imgsrc="https://cdn.pixabay.com/photo/2016/06/13/09/57/meeting-1453895__340.png"
-                visit="Contact Us"
+                visit="Services"
                 visitLink="/service"
             />
         </>
--- src/Common.jsx
+++ src/Common.jsx
@@ -10,9 +10,9 @@ const Common = (props) => {
                     <div className="col-10 mx-auto">
                     <div className="row">
                         <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                            <h1>{props.name}<strong className="brand-name">Tech Guru</strong></h1>
+                            <h1>{props.name}<strong className="brand-name"> Our Team</strong></h1>
                             <h2 className="my-3">
-                                We are the team of Advance Developers
+                                We are the team of Advance Web Developers.
                             </h2>
                             <div className="mt-3">
                                 <NavLink to={props.visitLink} className="btn btn-outline-primary">{props.visit}</NavLink>
